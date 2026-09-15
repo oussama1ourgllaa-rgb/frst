0a586662ff71fc0073578b2ac72ed33834c332dc
oussama ouregllaa <oussama1ourgllaa@gmail.com>
2026-09-15 13:03:19 +0100

    v2

---
+++ src/Salution.jsx
@@ -0,0 +1,9 @@
+
+export const Salution = ({person}) => {
+    return (
+        <p>
+            bonjour {person.nom} {person.prenom}
+        </p>
+    )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Counter from './counter.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { Salution } from './Salution.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Counter />
-  </StrictMode>,
+    <StrictMode>
+        <Salution person={{nom: 'Doe', prenom: 'John'}} /> 
+    </StrictMode>
 )
+
+
